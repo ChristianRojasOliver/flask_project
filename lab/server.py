f727cb19187e6cf12f176f78eaca45dac2f23092
ChristianRojasOliver <christian.rojas201@gmail.com>
2025-01-08 16:27:21 -0500

    send costum http

--- lab/server.py
+++ lab/server.py
@@ -10,3 +10,8 @@ def index():
     # Function that handles requests to the root URL
     # Return a plain text response
     return {"message":"hello world"}
+
+@app.route("/no_content")
+def no_content():
+    # Send custom HTTP code back with a tuple
+    return ("No content found", 204)
